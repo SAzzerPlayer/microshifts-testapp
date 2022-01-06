bc4727391a6a1a1ab54b7f9521779877d14756ca
Danyil Lohvyn <logvin24820@gmail.com>
2022-01-06 09:12:45 +0200

    Update random emoji loading process

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
 import "./App.css";
-import { InfoTable } from "./components/InfoTable";
-import { getRandomEmoji } from "./helpers/getRandomEmoji";
-import { EmojisSelector } from "./components/EmojisSelector";
+import {InfoTable} from "./components/InfoTable";
+import {getRandomEmoji} from "./helpers/getRandomEmoji";
+import {EmojisSelector} from "./components/EmojisSelector";
 
 function App() {
   const [selectedEmoji, selectEmoji] = React.useState();
 
   const requestRandomEmoji = React.useCallback(async () => {
     const emoji = await getRandomEmoji();
-    if (!selectedEmoji) {
-      selectEmoji(emoji);
-    }
-  }, [selectedEmoji]);
+    selectEmoji(emoji);
+  }, []);
 
-  //app starts with random emoji
-  React.useEffect(requestRandomEmoji, []); //eslint-disable-line
+  React.useEffect(requestRandomEmoji, [requestRandomEmoji]);
 
   return (
-    <div className="App">
-      <InfoTable emoji={selectedEmoji} />
-      <EmojisSelector onSelectEmoji={selectEmoji} />
-    </div>
+      <div className="App">
+        <InfoTable emoji={selectedEmoji}/>
+        <EmojisSelector onSelectEmoji={selectEmoji}/>
+      </div>
   );
 }
 
